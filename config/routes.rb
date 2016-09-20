2b769a635bee953d2796757f9e8031f57e2179d4
Michael Wright <michael@pixelfast.com>
2016-09-19 21:55:49 -0700

    fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pins
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users
   root "pages#home" #creates root_path
   get "about" => "pages#about" # creates about_path
 
