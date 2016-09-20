642cf34c8c927e41aa74b0581eb124a14c05a928
Michael Wright <michael@pixelfast.com>
2016-09-19 19:40:05 -0700

    Configured omniauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   resources :pins
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root "pages#home" #creates root_path
   get "about" => "pages#about" # creates about_path
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  # config/routes.rb
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
+  # The priority is
+   based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
