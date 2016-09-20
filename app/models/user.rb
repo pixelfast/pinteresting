5503d0f3380fea15bf5db042ef3588047ec4bb4b
Michael Wright <michael@pixelfast.com>
2016-09-19 22:15:42 -0700

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
 
   # enforce strong password
   validates :password, :presence => true,
@@ -31,11 +33,12 @@ def self.from_omniauth(auth)
   end
 end
 
-  # class User < ActiveRecord::Base
-  #   def self.new_with_session(params, session)
-  #       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-  #         user.email = data["email"] if user.email.blank?
-  #       end
-   #    end
-   #  end
-  # end
+class User < ActiveRecord::Base
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+end
